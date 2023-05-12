72fcd41d1921dd353175dd1c2732d706780b5509
KaranamSireesha <karanamsireesha9@gmail.com>
2023-05-12 16:15:40 +0530

    updated code

--- redbus.js
+++ redbus.js
@@ -1,29 +1,26 @@
-const passenger = document.querySelector('.passenger')
+const passenger = document.querySelector(".passenger");
 const count = document.getElementById("count");
 const busType = document.getElementById("bus");
-var numberofSeatsBooked=document.getElementById("numberofSeats");
+var numberofSeatsBooked = document.getElementById("numberofSeats");
 var totalAmount = document.getElementById("total");
-c=0;
+c = 0;
 
 let ticketPrice = +busType.value;
 
 function buttonUpdate() {
-
   const button = document.createElement("button");
   button.textContent = "Proceed to Pay";
 
   let buttonContainer = document.getElementById("button-container");
-  buttonContainer.innerHTML = ''; 
+  buttonContainer.innerHTML = "";
   buttonContainer.appendChild(button);
 
-  buttonContainer.addEventListener('click', function(){
-    // buttonContainer = alert("Your ticket booking is successfull. Happy Journey!");
-    window.location.href = "./index2.html"
-  })
+  buttonContainer.addEventListener("click", function () {
+    window.location.href = "./index2.html";
+  });
 }
 
-
-busType.addEventListener('change', e => {
+busType.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
 });
 
@@ -31,27 +28,38 @@ var image = document.querySelectorAll(".seat.seat-img.image");
 for (let i = 0; i < image.length; i++) {
   image[i].onclick = function () {
     this.src = "./assets/MicrosoftTeams-image (3).png";
-    c=c+1;
+    c = c + 1;
     updateDetails(c);
+
+    this.onclick = null;
   };
 }
 
-
-
-var imageF = document.querySelectorAll(".seat.seat-img.imageF, .seat.seat-img1.imageF");
+var imageF = document.querySelectorAll(
+  ".seat.seat-img.imageF, .seat.seat-img1.imageF"
+);
 for (let i = 0; i < imageF.length; i++) {
   imageF[i].onclick = function () {
-    this.src = "./assets/Seat-pink.png";
-    c=c+1;
-    updateDetails(c);
+    let userInput = prompt("Please enter your gender:");
+
+    if (userInput === "female") {
+      this.src = "./assets/Seat-pink.png";
+
+      c = c + 1;
+      updateDetails(c);
+
+      this.onclick = null;
+    } else {
+      imageF[i] = alert("This seat is for female passengers");
+    }
   };
 }
 
-function updateDetails(){
-  numberofSeatsBooked.innerHTML=`no of seats booked ${c}`
-  totalAmount.innerHTML = `${ticketPrice*c}`
-  if(c>0){
-    setTimeout( buttonUpdate,300);
+function updateDetails() {
+  numberofSeatsBooked.innerHTML = `no of seats booked ${c}`;
+  totalAmount.innerHTML = `${ticketPrice * c}`;
+  if (c > 0) {
+    setTimeout(buttonUpdate, 300);
   }
 }
 
